f22b401a66e091fe67380cdafc26365f3c41b442
Ian Ross <ian@skybluetrades.net>
2015-02-20 12:23:02 +0100

    Tidying

--- genie-lib/libnc1/open_file_nc.f90
+++ genie-lib/libnc1/open_file_nc.f90
@@ -2,27 +2,27 @@
 ! Marc 1/4/03
 ! This file just opens a netcdf file
 !========================================
-      subroutine open_file_nc(filein, ncid)
-      implicit none
-!----------------------------------------
-! Define variables in the subroutine header
-!----------------------------------------
-      character(*), intent(in) :: filein    ! Name of file to open
-      integer, intent(out)     :: ncid      ! netCDF dataset ID
-!----------------------------------------
-! Define the other netcdf variables
-!----------------------------------------
-      integer :: status                      ! return code
-!----------------------------------------
-! Include files
-!----------------------------------------
-      include 'netcdf.inc'
-!----------------------------------------
-! open the netcdf file
-!----------------------------------------
-      status=nf_open(filein, nf_nowrite, ncid)
-      if (status .ne. nf_noerr) then
-         write(6, *) 'ERROR: opening ', filein
-         stop
-      endif
-      end
+subroutine open_file_nc(filein, ncid)
+  implicit none
+  !----------------------------------------
+  ! Define variables in the subroutine header
+  !----------------------------------------
+  character(*), intent(in) :: filein    ! Name of file to open
+  integer, intent(out)     :: ncid      ! netCDF dataset ID
+  !----------------------------------------
+  ! Define the other netcdf variables
+  !----------------------------------------
+  integer :: status                      ! return code
+  !----------------------------------------
+  ! Include files
+  !----------------------------------------
+  include 'netcdf.inc'
+  !----------------------------------------
+  ! open the netcdf file
+  !----------------------------------------
+  status=nf_open(filein, nf_nowrite, ncid)
+  if (status .ne. nf_noerr) then
+     write(6, *) 'ERROR: opening ', filein
+     stop
+  endif
+end subroutine open_file_nc
--- genie-lib/libnc1/writenc6.f90
+++ genie-lib/libnc1/writenc6.f90
@@ -2,7 +2,6 @@ SUBROUTINE ininc(fname, nmaxdims, ndim, nvar, natts, nattsvar, &
      & vdims, vadims, ndims, dimname, varname, &
      & attdimname, attvarname, ncid, iddim, idvar)
   IMPLICIT NONE
-
   CHARACTER(LEN=*), INTENT(IN) :: fname
   INTEGER, INTENT(IN) :: nmaxdims, ndim, nvar
   INTEGER, DIMENSION(1:ndim), INTENT(IN) :: ndims, natts
@@ -30,264 +29,210 @@ SUBROUTINE ininc(fname, nmaxdims, ndim, nvar, natts, nattsvar, &
   iflen = lnsig(fname)
 
   ! Enter define mode
-  if (idebug.eq.1) then
-     print*,' opening file = ',fname(1:iflen)
-  end if
-  iret = nf_create(fname(1:iflen), NF_CLOBBER, ncid)
-  call check_err(iret)
-  !
+  IF (idebug == 1) THEN
+     PRINT *, ' opening file = ', fname(1:iflen)
+  END IF
+  iret = NF_CREATE(fname(1:iflen), NF_CLOBBER, ncid)
+  CALL check_err(iret)
+
   ! define dimensions
-  !
-  if (idebug.eq.1) then
-     print*,' defining dimensions ',ndim
-  end if
-  do ii=1,ndim
-     tname=dimname(ii)
-     itlen=lnsig(tname)
-     iret = nf_def_dim(ncid,tname(1:itlen),ndims(ii),dimdim(ii))
-  end do
-  !
+  IF (idebug == 1) PRINT *, ' defining dimensions ', ndim
+  DO ii = 1, ndim
+     tname = dimname(ii)
+     itlen = lnsig(tname)
+     iret = NF_DEF_DIM(ncid, tname(1:itlen), ndims(ii), dimdim(ii))
+  END DO
+
   ! define coordinates etc
-  !
-  if (idebug.eq.1) then
-     print*,' defining coordinates ',ndim
-  end if
-  do ii=1,ndim
-     tname=dimname(ii)
-     itlen=lnsig(tname)
+  IF (idebug == 1) PRINT *, ' defining coordinates ', ndim
+  DO ii = 1, ndim
+     tname = dimname(ii)
+     itlen = lnsig(tname)
      tempdims(1) = dimdim(ii)
-     iret = nf_def_var(ncid,tname(1:itlen), NF_REAL, 1, tempdims,iddim(ii))
-     call check_err(iret)
-  end do
-  !
+     iret = NF_DEF_VAR(ncid, tname(1:itlen), NF_REAL, 1, tempdims, iddim(ii))
+     CALL check_err(iret)
+  END DO
+
   ! and the real data
-  !
-  if (idebug.eq.1) then
-     print*,' defining variables ',nvar
-  end if
-  do ii=1,nvar
-     tname=varname(ii)
-     itlen=lnsig(tname)
-     if (idebug.eq.1) then
-        print*,'       variables ',ii,vdims(ii),tname(1:itlen)
-     end if
-     do iii=1,vdims(ii)
-        tempdims(iii)=dimdim(vadims(iii,ii))
-        if (idebug.eq.1) then
-           print*,'       variables ',ii,iii,tempdims(iii)
-        end if
-     end do
-     iret = nf_def_var(ncid,tname(1:itlen), NF_REAL,vdims(ii), &
+  IF (idebug == 1) PRINT *, ' defining variables ', nvar
+  DO ii = 1, nvar
+     tname = varname(ii)
+     itlen = lnsig(tname)
+     IF (idebug == 1) &
+          & PRINT *, '       variables ', ii, vdims(ii), tname(1:itlen)
+     DO iii = 1, vdims(ii)
+        tempdims(iii) = dimdim(vadims(iii,ii))
+        IF (idebug == 1) PRINT *, '       variables ', ii, iii, tempdims(iii)
+     END DO
+     iret = NF_DEF_VAR(ncid,tname(1:itlen), NF_REAL, vdims(ii), &
           & tempdims,idvar(ii))
-     call check_err(iret)
-  end do
-  !
+     CALL check_err(iret)
+  END DO
+
   ! assign attributes for coordinates
-  !
-  if (idebug.eq.1) then
-     print*,' Defining attibutes of coordinates ',ndim
-  end if
-  do ii=1,ndim
-     if (idebug.eq.1) then
-        print*,' Number of attributes = ',ii,natts(ii)
-     end if
-     do iii=1,natts(ii)
-        tname1=attdimname(1,iii,ii)
-        tname2=attdimname(2,iii,ii)
-        itlen1=lnsig(tname1)
-        itlen2=lnsig(tname2)
-        if (idebug.eq.1) then
-           print*,ii,iii,tname1(1:itlen1)
-           print*,ii,iii,tname2(1:itlen2)
-        end if
-        iret = nf_put_att_text(ncid,iddim(ii), &
-             & tname1(1:itlen1),itlen2, &
-             & tname2(1:itlen2))
-        call check_err(iret)
-     end do
+  IF (idebug == 1) PRINT *, ' Defining attibutes of coordinates ', ndim
+  DO ii = 1, ndim
+     IF (idebug == 1) PRINT *, ' Number of attributes = ', ii, natts(ii)
+     DO iii = 1, natts(ii)
+        tname1 = attdimname(1,iii,ii)
+        tname2 = attdimname(2,iii,ii)
+        itlen1 = lnsig(tname1)
+        itlen2 = lnsig(tname2)
+        IF (idebug == 1) THEN
+           PRINT *, ii, iii, tname1(1:itlen1)
+           PRINT *, ii, iii, tname2(1:itlen2)
+        END IF
+        iret = NF_PUT_ATT_TEXT(ncid, iddim(ii), &
+             & tname1(1:itlen1), itlen2, tname2(1:itlen2))
+        CALL check_err(iret)
+     END DO
      realval(1) = -99999.0
-     if (realkind.eq.4) then
-        iret = nf_put_att_real(ncid,iddim(ii), 'missing_value', &
+     IF (realkind == 4) THEN
+        iret = NF_PUT_ATT_REAL(ncid,iddim(ii), 'missing_value', &
              & NF_REAL, 1, realval)
-     elseif (realkind.eq.8) then
-        iret = nf_put_att_double(ncid,iddim(ii), 'missing_value', &
+     ELSE IF (realkind == 8) THEN
+        iret = NF_PUT_ATT_DOUBLE(ncid,iddim(ii), 'missing_value', &
              & NF_REAL, 1, realval)
-     else
-        print*,'precision problem in writenc6'
-        stop
-     endif
-     call check_err(iret)
-  end do
-  !
-  !  assign attributes for variables
-  !
-  if (idebug.eq.1) then
-     print*,' Defining attibutes of variables ',nvar
-  end if
-  do ii=1,nvar
-     if (idebug.eq.1) then
-        print*,' Number of attributes = ',ii,nattsvar(ii)
-     end if
-     do iii=1,nattsvar(ii)
-        tname1=attvarname(1,iii,ii)
-        tname2=attvarname(2,iii,ii)
-        itlen1=lnsig(tname1)
-        itlen2=lnsig(tname2)
-        if (idebug.eq.1) then
-           print*,ii,iii,tname1(1:itlen1)
-           print*,ii,iii,tname2(1:itlen2)
-        end if
-        iret = nf_put_att_text(ncid,idvar(ii), &
-             & tname1(1:itlen1),itlen2, &
-             & tname2(1:itlen2))
-        call check_err(iret)
-     end do
+     ELSE
+        PRINT *, 'precision problem in writenc6'
+        STOP
+     END IF
+     CALL check_err(iret)
+  END DO
+
+  ! assign attributes for variables
+  IF (idebug == 1) PRINT *, ' Defining attibutes of variables ', nvar
+  DO ii = 1, nvar
+     IF (idebug == 1) PRINT *, ' Number of attributes = ', ii, nattsvar(ii)
+     DO iii = 1, nattsvar(ii)
+        tname1 = attvarname(1,iii,ii)
+        tname2 = attvarname(2,iii,ii)
+        itlen1 = lnsig(tname1)
+        itlen2 = lnsig(tname2)
+        IF (idebug == 1) THEN
+           PRINT *, ii, iii, tname1(1:itlen1)
+           PRINT *, ii, iii, tname2(1:itlen2)
+        END IF
+        iret = NF_PUT_ATT_TEXT(ncid,idvar(ii), &
+             & tname1(1:itlen1),itlen2, tname2(1:itlen2))
+        CALL check_err(iret)
+     END DO
      realval(1) = -99999.0
-     if (realkind.eq.4) then
-        iret = nf_put_att_real(ncid,idvar(ii), 'missing_value', &
+     IF (realkind == 4) THEN
+        iret = NF_PUT_ATT_REAL(ncid,idvar(ii), 'missing_value', &
              & NF_REAL, 1, realval)
-     elseif (realkind.eq.8) then
-        iret = nf_put_att_double(ncid,idvar(ii), 'missing_value', &
+     ELSE IF (realkind == 8) THEN
+        iret = NF_PUT_ATT_DOUBLE(ncid,idvar(ii), 'missing_value', &
              & NF_REAL, 1, realval)
-     else
-        print*,'precision problem in writenc6'
-        stop
-     endif
-     call check_err(iret)
-  end do
-  !
+     ELSE
+        PRINT *, 'precision problem in writenc6'
+        STOP
+     END IF
+     CALL check_err(iret)
+  END DO
+
   ! global attribute
-  !
-  if (idebug.eq.1) then
-     print*,' Writing global attribute '
-  end if
-  tname='Produced using writenc6 program by PJV'
-  itlen=lnsig(tname)
-  iret = nf_put_att_text(ncid, NF_GLOBAL,'title', itlen,tname(1:itlen))
-  call check_err(iret)
-  !
+  IF (idebug == 1) PRINT *, ' Writing global attribute '
+  tname = 'Produced using writenc6 program by PJV'
+  itlen = lnsig(tname)
+  iret = NF_PUT_ATT_TEXT(ncid, NF_GLOBAL,'title', itlen,tname(1:itlen))
+  CALL check_err(iret)
+
   ! leave define mode
-  !
-  iret = nf_enddef(ncid)
-  call check_err(iret)
+  iret = NF_ENDDEF(ncid)
+  CALL check_err(iret)
 
-  if (idebug.eq.1) then
-     print*,' Finished ininc '
-  end if
-
-  return
-end subroutine ininc
+  IF (idebug == 1) PRINT *, ' Finished ininc '
+END SUBROUTINE ininc
 
 
-subroutine writedim(ncid,iddim,data)
-  implicit none
-  include 'netcdf.inc'
-  integer ncid,iddim
-  real data(*)
+SUBROUTINE writedim(ncid, iddim, data)
+  IMPLICIT NONE
+  INCLUDE 'netcdf.inc'
+  INTEGER, INTENT(IN) :: ncid, iddim
+  REAL, INTENT(IN) :: data(*)
 
-  integer iret
+  INTEGER :: iret, realkind
 
-  integer realkind
-
-  realkind=kind(data(1))
+  realkind = KIND(data(1))
 
   ! write coordinates
-  !
-  if (realkind.eq.4) then
-     iret = nf_put_var_real(ncid,iddim,data)
-  else if (realkind.eq.8) then
-     iret = nf_put_var_double(ncid,iddim,data)
-  else
-     print*,'precision problem in writedim'
-     stop
-  endif
-  call check_err(iret)
-
-  return
-end subroutine writedim
+  IF (realkind == 4) THEN
+     iret = NF_PUT_VAR_REAL(ncid, iddim, data)
+  ELSE IF (realkind == 8) THEN
+     iret = NF_PUT_VAR_DOUBLE(ncid, iddim, data)
+  ELSE
+     PRINT *, 'precision problem in writedim'
+     STOP
+  END IF
+  CALL check_err(iret)
+END SUBROUTINE writedim
 
 
-subroutine writevar(ncid,idvar,data)
-  implicit none
-  include 'netcdf.inc'
-  integer ncid,idvar
-  real data(*)
+SUBROUTINE writevar(ncid, idvar, data)
+  IMPLICIT NONE
+  INCLUDE 'netcdf.inc'
+  INTEGER, INTENT(IN) :: ncid, idvar
+  REAL, INTENT(IN) :: data(*)
 
-  integer iret
-
-  integer realkind
+  INTEGER :: iret, realkind
 
   ! write data
-  realkind=kind(data(1))
+  realkind = KIND(data(1))
+  IF (realkind == 4) THEN
+     iret = NF_PUT_VAR_REAL(ncid, idvar, data)
+  ELSE IF (realkind == 8) THEN
+     iret = NF_PUT_VAR_DOUBLE(ncid, idvar, data)
+  ELSE
+     PRINT *, 'precision problem in writevar'
+     STOP
+  END IF
+  CALL check_err(iret)
+END SUBROUTINE writevar
 
 
-  if (realkind.eq.4) then
-     iret = nf_put_var_real(ncid,idvar,data)
-  else if (realkind.eq.8) then
-     iret = nf_put_var_double(ncid,idvar,data)
-  else
-     print*,'precision problem in writevar'
-     stop
-  endif
-  call check_err(iret)
+SUBROUTINE writevar2(ncid, idvar, data, ix1, ix2, iy1, iy2, iz1, iz2, it1, it2)
+  IMPLICIT NONE
+  INCLUDE 'netcdf.inc'
+  INTEGER, INTENT(IN) :: ncid, idvar
+  INTEGER, INTENT(IN) :: ix1, ix2, iy1, iy2, iz1, iz2, it1, it2
+  REAL, INTENT(IN) :: data(*)
 
-  return
-end subroutine writevar
-
-
-subroutine writevar2(ncid,idvar,data,ix1,ix2,iy1,iy2,iz1,iz2,it1,it2)
-  implicit none
-  include 'netcdf.inc'
-  integer ncid,idvar
-  integer ix1,ix2,iy1,iy2,iz1,iz2,it1,it2
-  real data(*)
-  integer start(4),count(4)
-
-  integer iret
-
-  integer realkind
+  INTEGER, DIMENSION(4) :: start, count
+  INTEGER :: iret, realkind
 
   ! write data
-  realkind=kind(data(1))
+  realkind = KIND(data(1))
+  start(1) = ix1
+  start(2) = iy1
+  start(3) = iz1
+  IF (it1 > 0) start(4) = it1
+  start(4) = it1
+  count(1) = ix2 - ix1 + 1
+  count(2) = iy2 - iy1 + 1
+  count(3) = iz2 - iz1 + 1
+  IF (it1 > 0) count(4) = it2 - it1 + 1
 
-  start(1)=ix1
-  start(2)=iy1
-  start(3)=iz1
-  if (it1.gt.0) then
-     start(4)=it1
-  end if
-  start(4)=it1
-  count(1)=ix2-ix1+1
-  count(2)=iy2-iy1+1
-  count(3)=iz2-iz1+1
-  if (it1.gt.0) then
-     count(4)=it2-it1+1
-  end if
+  IF (realkind == 4) THEN
+     iret = NF_PUT_VARA_REAL(ncid, idvar, start,count, data)
+  ELSE IF (realkind == 8) THEN
+     iret = NF_PUT_VARA_DOUBLE(ncid, idvar, start, count, data)
+  ELSE
+     PRINT *, 'precision problem in writevar2'
+  END IF
 
-  if (realkind.eq.4) then
-     iret = nf_put_vara_real(ncid,idvar,start,count,data)
-  else if (realkind.eq.8) then
-     iret = nf_put_vara_double(ncid,idvar,start,count,data)
-  else
-     print*,'precision problem in writevar2'
-     stop
-  endif
-
-  call check_err(iret)
-
-  return
-end subroutine writevar2
+  CALL check_err(iret)
+END SUBROUTINE writevar2
 
 
-!
 ! close file
-!
-subroutine closenc(ncid)
-  implicit none
-  include 'netcdf.inc'
-  integer ncid,iret
-  iret = nf_close(ncid)
-  call check_err(iret)
+SUBROUTINE closenc(ncid)
+  IMPLICIT NONE
+  INCLUDE 'netcdf.inc'
+  INTEGER, INTENT(IN) :: ncid
 
-  return
-end subroutine closenc
+  INTEGER :: iret
+
+  iret = NF_CLOSE(ncid)
+  CALL check_err(iret)
+END SUBROUTINE closenc
