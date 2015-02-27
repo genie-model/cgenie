11713484609b89a34410dbf675d1666582bd8ebe
Ian Ross <ian@skybluetrades.net>
2015-02-27 17:49:03 +0100

    Data and test repo downloads in setup script

--- setup-cgenie.py
+++ setup-cgenie.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python2
 
 import os
+import subprocess as sp
 import json
 import utils
 
+datarepo = 'git@github.com:ian-ross/cgenie-data'
+testrepo = 'git@github.com:ian-ross/cgenie-test'
+
 def ask(prompt, default):
     return raw_input(prompt + " [" + default + "]: ") or default
+
+def yesno(prompt, default):
+    opts = 'Yn' if default else 'yN'
+    return raw_input(prompt + " [" + opts + "]: ") or default
 
 config = utils.read_cgenie_config()
 if config:
@@ -21,3 +29,19 @@ else:
                'cgenie_version': vers }
     with open(utils.genie_cfgfile, 'w') as f:
         json.dump(config, f)
+
+download_data = False
+download_test = False
+if not os.path.exists(data):
+    download_data = yesno('Data directory does not exist. Download?', True)
+if not os.path.exists(test):
+    download_test = yesno('Test directory does not exist. Download?', True)
+
+if download_data:
+    print('Downloading cgenie-data repository...')
+    if sp.call(['git', 'clone', datarepo, data]) != 0:
+        print('FAILED TO CLONE cgenie-data REPOSITORY!')
+if download_test:
+    print('Downloading cgenie-test repository...')
+    if sp.call(['git', 'clone', testrepo, test]) != 0:
+        print('FAILED TO CLONE cgenie-test REPOSITORY!')
