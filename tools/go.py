844575d3a8c69a19fc4d2048d726ce49d6d432e9
Ian Ross <ian@skybluetrades.net>
2015-03-26 12:14:09 +0100

    Protect against non-GUI Python installs

--- tools/go.py
+++ tools/go.py
@@ -4,192 +4,199 @@ from __future__ import print_function
 import os, os.path, sys, shutil, argparse, glob
 import subprocess as sp
 import platform as plat
-import Queue as qu
-import threading as thr
-import Tkinter as tk
-import tkFont
-import tkMessageBox
-import ttk
+try:
+    import Queue as qu
+    import threading as thr
+    import Tkinter as tk
+    import tkFont
+    import tkMessageBox
+    import ttk
+    gui_available = True
+except:
+    gui_available = False
 
 import utils as U
 
 
-# GUI CODE
+if gui_available:
+    # GUI CODE
 
-def iter_except(function, exception):
-    """Works like builtin 2-argument `iter()`, but stops on `exception`."""
-    try:
-        while True:
-            yield function()
-    except exception:
-        return
-
-def kill_after(r, p, countdown):
-    if p.poll() is None:
-        countdown -= 1
-        if countdown < 0:
-            p.kill()
-        else:
-            r.after(1000, kill_after, r, p, countdown)
+    def iter_except(function, exception):
+        """Works like builtin 2-argument `iter()`, but stops on `exception`."""
+        try:
+            while True:
+                yield function()
+        except exception:
             return
-    p.wait()
 
-class Application(tk.Frame):
-    def clear(self):
-        self.stop()
-        self.text.delete('1.0', tk.END)
-
-    def clean(self):
-        self.clear()
-        clean(False)
-
-    def cleaner(self):
-        self.clear()
-        clean(True)
-
-    def build(self):
-        self.clear()
-        self.buttons_active(False)
-        build(lambda: self.buttons_active(True))
-
-    def run(self):
-        self.clear()
-        self.buttons_active(False)
-        def c():
-            run(lambda: self.buttons_active(True))
-        build(c)
-
-    def stop(self):
-        if self.proc:
-            p = self.proc
-            self.proc = None
-            self.cont = None
-            self.contrest = None
-            p.terminate()
-            kill_after(self.root, p, countdown=5)
-            message('CANCELLED')
-            self.buttons_active(True)
-
-    def done(self):
-        self.stop()
-        self.quit()
-
-    def message(self, s):
-        self.stop()
-        self.text.insert(tk.END, 79 * '*' + '\n')
-        self.text.insert(tk.END, '\n')
-        self.text.insert(tk.END, '    ' + s + '\n')
-        self.text.insert(tk.END, '\n')
-        self.text.insert(tk.END, 79 * '*' + '\n')
-        self.text.insert(tk.END, '\n')
-        self.text.see(tk.END)
-
-    def line(self, s):
-        self.text.insert(tk.END, s + '\n')
-        self.text.see(tk.END)
-
-    def createWidgets(self):
-        self.job_label_font = tkFont.Font(weight='bold')
-        job = os.path.relpath(os.curdir, U.cgenie_jobs)
-        self.job_label = ttk.Label(self, text='Job: ' + job,
-                                   font=self.job_label_font)
-        self.button_frame = ttk.Frame(self)
-        self.content_frame = ttk.Frame(self)
-        self.action_buttons = [ ]
-        action_button_defs = [['Run',     self.run],
-                              ['Build',   self.build],
-                              ['Clean',   self.clean],
-                              ['Cleaner', self.cleaner]]
-        for r in range(len(action_button_defs)):
-            lab, act = action_button_defs[r]
-            self.action_buttons.append(ttk.Button(self.button_frame,
-                                                  text=lab, command=act))
-        self.quit_button = ttk.Button(self.button_frame,
-                                      text='Quit', command=self.done)
-        self.cancel_button = ttk.Button(self.button_frame, text='Cancel',
-                                        command=self.stop)
-        self.text = tk.Text(self.content_frame)
-        self.text_scroll = ttk.Scrollbar(self.content_frame,
-                                         command=self.text.yview)
-        self.text['yscrollcommand'] = self.text_scroll.set
-
-        self.winfo_toplevel().rowconfigure(0, weight=1)
-        self.winfo_toplevel().columnconfigure(0, weight=1)
-        self.grid(sticky=tk.N+tk.E+tk.S+tk.W)
-        self.columnconfigure(0, weight=0)
-        self.columnconfigure(1, weight=1)
-        self.rowconfigure(0, weight=0)
-        self.rowconfigure(1, weight=1)
-        self.job_label.grid(column=0, row=0, columnspan=2, sticky=tk.W,
-                            padx=10, pady=5)
-        self.button_frame.grid(column=0, row=1, sticky=tk.N+tk.W, pady=5)
-        self.content_frame.rowconfigure(0, weight=1)
-        self.content_frame.columnconfigure(0, weight=1)
-        self.content_frame.grid(column=1, row=1,
-                                sticky=tk.N+tk.E+tk.S+tk.W, padx=5)
-        self.text.grid(column=0, row=0, sticky=tk.N+tk.E+tk.S+tk.W, pady=5)
-        self.text.rowconfigure(0, weight=1)
-        self.text.columnconfigure(0, weight=1)
-        self.text_scroll.grid(column=1, row=0, sticky=tk.N+tk.S, pady=5)
-        for r in range(len(action_button_defs)):
-            self.action_buttons[r].grid(column=0, row=r,
-                                        sticky=tk.E+tk.W, padx=5)
-        self.quit_button.grid(column=0, row=len(action_button_defs),
-                              sticky=tk.E+tk.W, padx=5)
-        self.cancel_button.grid(column=0, row=len(action_button_defs)+1,
-                                sticky=tk.E+tk.W+tk.S, padx=5, pady=10)
-
-    def buttons_active(self, state):
-        s = ['disabled'] if not state else ['!disabled']
-        for b in self.action_buttons: b.state(s)
-
-    def __init__(self, root=None):
-        self.root = root
-        self.proc = None
-        tk.Frame.__init__(self, root)
-        self.createWidgets()
-
-    # Manage subsidiary process.
-    def manage(self, cmd, logfp, cont, *rest):
-        self.cont = cont
-        self.contrest = rest
-        self.proc = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.STDOUT)
-        q = qu.Queue()
-        t = thr.Thread(target=self.reader_thread, args=[q, logfp]).start()
-        self.update(q)
-
-    # Background thread: reads from subsidiary process.
-    def reader_thread(self, q, logfp):
-        for line in iter(self.proc.stdout.readline, ''):
-            q.put(line)
-            logfp.write(line)
-        q.put(None)
-
-    # Runs in GUI thread...
-    def update(self, q):
-        for line in iter_except(q.get_nowait, qu.Empty):
-            if line is None:
-                status = self.proc.wait() if self.proc else -1
-                self.proc = None
-                if self.cont:
-                    cont = self.cont
-                    self.cont = None
-                    contrest = self.contrest if self.contrest else None
-                    self.contrest = None
-                    if contrest:
-                        cont(status, *contrest)
-                    else:
-                        cont(status)
-                return
+    def kill_after(r, p, countdown):
+        if p.poll() is None:
+            countdown -= 1
+            if countdown < 0:
+                p.kill()
             else:
-                self.text.insert(tk.END, line)
-                self.text.see(tk.END)
-        self.root.after(40, self.update, q)
+                r.after(1000, kill_after, r, p, countdown)
+                return
+        p.wait()
+
+    class Application(tk.Frame):
+        def clear(self):
+            self.stop()
+            self.text.delete('1.0', tk.END)
+
+        def clean(self):
+            self.clear()
+            clean(False)
+
+        def cleaner(self):
+            self.clear()
+            clean(True)
+
+        def build(self):
+            self.clear()
+            self.buttons_active(False)
+            build(lambda: self.buttons_active(True))
+
+        def run(self):
+            self.clear()
+            self.buttons_active(False)
+            def c():
+                run(lambda: self.buttons_active(True))
+            build(c)
+
+        def stop(self):
+            if self.proc:
+                p = self.proc
+                self.proc = None
+                self.cont = None
+                self.contrest = None
+                p.terminate()
+                kill_after(self.root, p, countdown=5)
+                message('CANCELLED')
+                self.buttons_active(True)
+
+        def done(self):
+            self.stop()
+            self.quit()
+
+        def message(self, s):
+            self.stop()
+            self.text.insert(tk.END, 79 * '*' + '\n')
+            self.text.insert(tk.END, '\n')
+            self.text.insert(tk.END, '    ' + s + '\n')
+            self.text.insert(tk.END, '\n')
+            self.text.insert(tk.END, 79 * '*' + '\n')
+            self.text.insert(tk.END, '\n')
+            self.text.see(tk.END)
+
+        def line(self, s):
+            self.text.insert(tk.END, s + '\n')
+            self.text.see(tk.END)
+
+        def createWidgets(self):
+            self.job_label_font = tkFont.Font(weight='bold')
+            job = os.path.relpath(os.curdir, U.cgenie_jobs)
+            self.job_label = ttk.Label(self, text='Job: ' + job,
+                                       font=self.job_label_font)
+            self.button_frame = ttk.Frame(self)
+            self.content_frame = ttk.Frame(self)
+            self.action_buttons = [ ]
+            action_button_defs = [['Run',     self.run],
+                                  ['Build',   self.build],
+                                  ['Clean',   self.clean],
+                                  ['Cleaner', self.cleaner]]
+            for r in range(len(action_button_defs)):
+                lab, act = action_button_defs[r]
+                self.action_buttons.append(ttk.Button(self.button_frame,
+                                                      text=lab, command=act))
+            self.quit_button = ttk.Button(self.button_frame,
+                                          text='Quit', command=self.done)
+            self.cancel_button = ttk.Button(self.button_frame, text='Cancel',
+                                            command=self.stop)
+            self.text = tk.Text(self.content_frame)
+            self.text_scroll = ttk.Scrollbar(self.content_frame,
+                                             command=self.text.yview)
+            self.text['yscrollcommand'] = self.text_scroll.set
+
+            self.winfo_toplevel().rowconfigure(0, weight=1)
+            self.winfo_toplevel().columnconfigure(0, weight=1)
+            self.grid(sticky=tk.N+tk.E+tk.S+tk.W)
+            self.columnconfigure(0, weight=0)
+            self.columnconfigure(1, weight=1)
+            self.rowconfigure(0, weight=0)
+            self.rowconfigure(1, weight=1)
+            self.job_label.grid(column=0, row=0, columnspan=2, sticky=tk.W,
+                                padx=10, pady=5)
+            self.button_frame.grid(column=0, row=1, sticky=tk.N+tk.W, pady=5)
+            self.content_frame.rowconfigure(0, weight=1)
+            self.content_frame.columnconfigure(0, weight=1)
+            self.content_frame.grid(column=1, row=1,
+                                    sticky=tk.N+tk.E+tk.S+tk.W, padx=5)
+            self.text.grid(column=0, row=0, sticky=tk.N+tk.E+tk.S+tk.W, pady=5)
+            self.text.rowconfigure(0, weight=1)
+            self.text.columnconfigure(0, weight=1)
+            self.text_scroll.grid(column=1, row=0, sticky=tk.N+tk.S, pady=5)
+            for r in range(len(action_button_defs)):
+                self.action_buttons[r].grid(column=0, row=r,
+                                            sticky=tk.E+tk.W, padx=5)
+            self.quit_button.grid(column=0, row=len(action_button_defs),
+                                  sticky=tk.E+tk.W, padx=5)
+            self.cancel_button.grid(column=0, row=len(action_button_defs)+1,
+                                    sticky=tk.E+tk.W+tk.S, padx=5, pady=10)
+
+        def buttons_active(self, state):
+            s = ['disabled'] if not state else ['!disabled']
+            for b in self.action_buttons: b.state(s)
+
+        def __init__(self, root=None):
+            self.root = root
+            self.proc = None
+            tk.Frame.__init__(self, root)
+            self.createWidgets()
+
+        # Manage subsidiary process.
+        def manage(self, cmd, logfp, cont, *rest):
+            self.cont = cont
+            self.contrest = rest
+            self.proc = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.STDOUT)
+            q = qu.Queue()
+            t = thr.Thread(target=self.reader_thread, args=[q, logfp]).start()
+            self.update(q)
+
+        # Background thread: reads from subsidiary process.
+        def reader_thread(self, q, logfp):
+            for line in iter(self.proc.stdout.readline, ''):
+                q.put(line)
+                logfp.write(line)
+            q.put(None)
+
+        # Runs in GUI thread...
+        def update(self, q):
+            for line in iter_except(q.get_nowait, qu.Empty):
+                if line is None:
+                    status = self.proc.wait() if self.proc else -1
+                    self.proc = None
+                    if self.cont:
+                        cont = self.cont
+                        self.cont = None
+                        contrest = self.contrest if self.contrest else None
+                        self.contrest = None
+                        if contrest:
+                            cont(status, *contrest)
+                        else:
+                            cont(status)
+                    return
+                else:
+                    self.text.insert(tk.END, line)
+                    self.text.see(tk.END)
+            self.root.after(40, self.update, q)
 
 
 # This script can run as a GUI or command line tool.
 
 gui = True if len(sys.argv) == 1 else False
+if gui and not gui_available:
+    sys.exit('GUI operation is not available!')
 
 
 # GENIE configuration.
