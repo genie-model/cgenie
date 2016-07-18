54f63daa26de30b90d37818aeb7c8bb8deb9ddbb
TheMetalMonkey <toby.dunne@gmail.com>
2016-07-18 15:27:37 +0100

    Remove test print statement genie.f90

--- src/genie.f90
+++ src/genie.f90
@@ -33,7 +33,6 @@ PROGRAM GENIE
   PRINT *, '  *** Welcome to cGENIE -- version: ', TRIM(genie_version)
   PRINT *, '*******************************************************'
   PRINT *
-  PRINT *, 'MODIFIED IN JOB SRC test'
 #ifdef INTEL_PROFILE
   print *, 'Intel Profile API enabled'
   call itt_profile_init()
